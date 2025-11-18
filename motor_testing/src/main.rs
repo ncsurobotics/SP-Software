ef0a190bdfc85fd8a8bbedde02cce39f507c95e0
tlhart12 <tlhart365@gmail.com>
2025-11-17 20:17:18 -0500

    Add receiver functionality for IMU

--- motor_testing/src/main.rs
+++ motor_testing/src/main.rs
@@ -1,4 +1,5 @@
 use mavio::default_dialect::enums::mav_frame;
+use mavio::dialects::Common;
 use mavio::prelude::*;
 use mavio::default_dialect as dialect;
 use dialect::enums::{MavAutopilot, MavModeFlag, MavState, MavType, MavCmd};
@@ -35,8 +36,9 @@ use embedded_io_adapters;
 fn main() {
     // Create a TCP client sender
     let serial = serialport::new("/dev/cu.usbmodem21101", 57600).open().unwrap();
-    let writer = StdIoWriter::new(serial);
-    let mut sender = Sender::new(writer);
+
+    let mut sender = Sender::new(StdIoWriter::new(serial));
+    let mut receiver = Receiver::versionless(StdIoReader::new(serial));
 
     // Create an endpoint that represents a MAVLink device speaking `MAVLink 2` protocol
     let endpoint = Endpoint::v2(MavLinkId::new(15, 42));
@@ -48,7 +50,7 @@ fn main() {
     // }
 
     // Create a message
-    let message = dialect::messages::command_long::CommandLong {
+    let motor_message = dialect::messages::command_long::CommandLong {
         target_system: 1,
         target_component: 1,
         command: MavCmd::DoMotorTest,
@@ -61,14 +63,33 @@ fn main() {
         param7: 0.0,
         confirmation: 0,
     };
-    println!("MESSAGE: {message:?}");
+    println!("MOTOR MESSAGE:", {motor_message:?});
 
     for i in 0..10 {
+        // Receive the current MAVLink frame
+        let frame = receiver.recv_frame();
+
+        // Validate MAVLink frame
+        if let Err(err) = frame.validate_checksum::<Minimal>() {
+            eprintln!("Invalid checksum: {err:?}");
+            continue;
+        }
+        match Ok(frame.decode().unwrap()) {
+            Common::Attitude(msg) => {
+                println!("IMU Yaw (rad): {}", msg.yaw);
+                println!("IMU Pitch (rad): {}", msg.pitch);
+                println!("IMU Roll (rad): {}", msg.roll);
+            },
+        }
+        /* if let Ok(Common::Attitude(msg)) = frame.decode() {
+            println!("IMU Yaw (rad): {}", msg.yaw);
+        } */
+
         // Build the next frame for this endpoint.
         // All required fields will be populated, including frame sequence counter.
-        let frame = endpoint.next_frame(&message).unwrap();
+        let frame = endpoint.next_frame(&motor_message).unwrap();
 
-        sender.send(&frame).unwrap();
+        sender.send(&frame);
         println!("FRAME #{} sent: {:#?}", i, frame);
     }
 }
