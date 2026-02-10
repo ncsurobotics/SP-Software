c680fea077992ced634b63b3a2a16747627788e4
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2026-02-09 20:38:17 -0500

    Working stab2 demo

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use tokio::io::WriteHalf;
 
 #[tokio::main]
 async fn main() {
-    let board = ControlBoard::serial("/dev/cu.usbmodem2101").await.expect("Connection Failed");
-    
+    let board = ControlBoard::serial("/dev/serial/by-id/usb-STMicroelectronics_Control_Board_v2__Virtual_COM_Port__3631363230325101002A001F-if00").await.expect("Connection Failed");
+    board
+        .stability_2_speed_set(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        .await;
 }
