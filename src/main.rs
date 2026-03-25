9daaa8c72a97cf0c19b4f0ec5aa2515a4ef0b136
Shaurya <sbhatt3@ncsu.edu>
2026-03-24 20:58:58 -0400

    MOTORS MOVE YAY

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::time::Duration;
+
 use sw9s_lib::comms::control_board::ControlBoard;
 use tokio::io::WriteHalf;
+use tokio::time::sleep;
+
 
 #[tokio::main]
 async fn main() {
     let board = ControlBoard::serial("/dev/serial/by-id/usb-STMicroelectronics_Control_Board_v2__Virtual_COM_Port__3631363230325101002A001F-if00").await.expect("Connection Failed");
-    board
-        .stability_2_speed_set(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        .await;
+    let _ = board.raw_speed_set([0.2, 0.2, 0.3, 0.2, 0.1, 0.0, -0.1, -0.2]).await;
+    sleep(Duration::from_millis(2000)).await;
+    let _ = board.raw_speed_set([-0.2, -0.2, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2]).await;
+    sleep(Duration::from_millis(2000)).await;
+    let _ = board.raw_speed_set([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]).await;
 }
