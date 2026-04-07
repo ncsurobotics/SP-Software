f59b71a2d7a305cc1fdc8500041d804014cb32bc
ClimbingWolf <zpeverall1@gmail.com>
2026-04-06 20:54:11 -0400

    arm structure

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
+    
+    let arm_bool: bool = false;
+    tokio::spawn(async move {
+        while arm_bool != true {
+            sleep(Duration::from_secs(1)).await;
+        }
+        dbg!("I AM ARMED")
+        
+    });
+    //I don't really know how we're supposed to edit arm and also there is no meb on the raspberry pi
+
     let board = ControlBoard::serial("/dev/serial/by-id/usb-STMicroelectronics_Control_Board_v2__Virtual_COM_Port__3631363230325101002A001F-if00").await.expect("Connection Failed");
     let _ = board.raw_speed_set([0.2, 0.2, 0.3, 0.2, 0.1, 0.0, -0.1, -0.2]).await;
     sleep(Duration::from_millis(2000)).await;
